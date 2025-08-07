cd5734473730210aab7b58066f5adb5b777e89d9
Wisnu Sanjaya <wsanjaya69@gmail.com>
2025-08-07 10:25:26 +0700

    12324

--- slip.py
+++ slip.py
@@ -4,26 +4,32 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import cm
 
 # Baca data dan konversi ke list of dict
-df = pd.read_excel('gaji_juli.xlsx')
+df = pd.read_excel('gaji_contoh.xlsx')
 df.fillna(0, inplace=True)
 df.set_index(keys='nik', inplace=True)
 data = df.to_dict(orient='records')
-
+print(df.head(3))
 def multi_slip(data, filename="data/slip_gaji.pdf"):
-    c = canvas.Canvas(filename, pagesize=[25 * cm, 15 * cm])
-    width, height = 25 * cm, 15 * cm
-    text_width = c.stringWidth("SLIP GAJI KARYAWAN", "Helvetica-Bold", 16)
-    center = (24.13 * cm - text_width * cm ) / 2
-    print(text_width)
+    c = canvas.Canvas(filename, pagesize=letter)
+    width, height = letter
 
     for item in data:
         c.setFont("Helvetica-Bold", 16)
-        c.drawString(center * cm, height - 2 * cm, "SLIP GAJI KARYAWAN")
+        c.drawString(8 * cm, height - 2 * cm, "SLIP GAJI KARYAWAN")
 
+        #  Informasi pekerja
         c.setFont("Helvetica", 11)
-        c.drawString(2 * cm, height - 3 * cm, f"NAMA{' '* 15}:")
-        c.drawString(2 * cm, height - 3.5 * cm, f"LOKASI{' '* 15}:")
-        c.drawString(7 * cm, height - 3 * cm, f"{item['nama']}")
+        c.drawString(2 * cm, height - 3 * cm, f"Nama")
+        c.drawString(2 * cm, height - 3.5 * cm, f"Lokasi")
+        c.drawString(2 * cm, height - 4 * cm, f"Status")
+        c.drawString(7 * cm, height - 3 * cm, f" : {item['nama']}")
+        c.drawString(7 * cm, height - 3.5 * cm, f" : {item['lokasi']}")
+        c.drawString(7 * cm, height - 4 * cm, f" : {item['status']}")
+        
+
+        c.drawString(2 * cm, height - 7 * cm, f"PENERIMAAN")
+        c.drawString(2 * cm, height - 8 * cm, f"Gaji Pokok")
+        c.drawString(7 * cm, height - 8 * cm, f" : Rp. {item['basic']:,.2f}")
 
         # ⛳️ Halaman baru untuk setiap slip
         c.showPage()  # ← Ini penting!
