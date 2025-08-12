a64063d0f154c319f9ed5d398a23b68932214340
Wisnu Sanjaya <wsanjaya69@gmail.com>
2025-08-12 13:00:29 +0700

    yyeyeryer

--- refactor1.py
+++ refactor1.py
@@ -40,34 +40,62 @@ print(df.columns)
 c = canvas.Canvas(filename="data/contoh.pdf", pagesize=A4)
 width, height = A4
 
-def render_penerimaan(c, item, x, y_start):
+
+def render_header(c, item, x, start_y):
+    text_width = c.stringWidth("SLIP GAJI KARYAWAN", "Helvetica-Bold", 16)
+    center = (width - text_width) / 2
+    c.setFont("Helvetica-Bold", 16)
+    c.drawString(center, height - 1 * cm, "SLIP GAJI KARYAWAN")
+
+    c.setFont("Helvetica", 11)
+    c.drawString(x, start_y, ": Nama")
+    c.drawString(x + 5 * cm, start_y, f"{item['nama']}")
+    y = start_y - 0.5 * cm
+
+    c.setFont("Helvetica", 11)
+    c.drawString(x, y, "Lokasi")
+    c.drawString(x + 5 * cm, y, f": {item['lokasi']}")
+    y -= 0.5 * cm
+    
+    return y
+
+
+def render_penerimaan(c, item, x, start_y):
     c.setFont("Helvetica-Bold", 12)
-    c.drawString(x, y_start, "PENERIMAAN")
-    y = y_start - 0.5 * cm
+    c.drawString(x, start_y, "PENERIMAAN")
+    y = start_y - 0.5 * cm
 
     c.setFont("Helvetica", 11)
     c.drawString(x, y, "Gaji Pokok")
-    c.drawRightString(x + 12 * cm, y, f"Rp. {item['basic']:,.2f}")
+    c.drawRightString(x + 12 * cm, y, f": Rp. {item['basic']:,.2f}")
     y -= 0.5 * cm
 
     c.drawString(x, y, "Tunjangan Jabatan")
-    c.drawRightString(x + 12 * cm, y, f"Rp. {item['jabatan']:,.2f}")
+    c.drawRightString(x + 12 * cm, y, f": Rp. {item['jabatan']:,.2f}")
     y -= 0.5 * cm
 
-    # Tambah baris lain di sini tanpa takut
+    # Tambah baris lain di sini
     c.drawString(x, y, "Lembur")
-    c.drawRightString(x + 12 * cm, y, f"Rp. {item['lembur']:,.2f}")
+    c.drawRightString(x + 12 * cm, y, f": Rp. {item['lembur']:,.2f}")
     y -= 0.5 * cm
 
     return y  # supaya bagian selanjutnya bisa lanjut dari sini
+
+
+
 
 def multi_slip(data, filename="data/slip_gaji1.pdf"):
     c = canvas.Canvas(filename, pagesize=A4)
     width, height = A4
 
     for item in data:
-        render_penerimaan(c, item, width, height)
-        c.showPage()  # slip baru per orang
+        margin_x = 2 * cm
+        start_y = height - 3 * cm 
+        render_header(c, item, margin_x, start_y)
+        render_penerimaan(c, item, margin_x, render_header(c, item, margin_x, start_y))
+
+        c.showPage()
     c.save()
+
 
 multi_slip(data)
