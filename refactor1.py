a9e394519421b871be78e6a4006f84fbc1b53585
Wisnu Sanjaya <wsanjaya69@gmail.com>
2025-08-14 15:08:26 +0700

    jsndjandiqwniud

--- refactor1.py
+++ refactor1.py
@@ -2,7 +2,9 @@ import pandas as pd
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import cm
+from reportlab.lib import colors
 from datetime import date
+from decimal import Decimal, ROUND_DOWN
 
 bulan_indonesia = [
     "", "Januari", "Februari", "Maret", "April", "Mei", "Juni",
@@ -40,26 +42,63 @@ print(df.columns)
 c = canvas.Canvas(filename="data/contoh.pdf", pagesize=A4)
 width, height = A4
 
+def render_kop(c):
+    c.drawImage("logo1.jpg", # Path file gambar
+                3 * cm, # Koordinat x
+                height - 2.8 * cm, # Koordinat y
+                width= 2.3 * cm, # lebar gambar dalam cm
+                height= 2.3 * cm, # Tinggi gambar dalam cm
+                preserveAspectRatio=True,
+                mask="auto"
+    )
+    x = 6 * cm
+    y = height - 1 * cm
+    c.setFont("Times-Roman", 16)
+    c.setFillColor(colors.red)
+    c.drawString(x, y, "PT. ALKATRA")
+    c.setFillColor(colors.black)
+    y -= 0.5 * cm
+
+    c.setFont("Times-Roman", 12)
+    c.drawString(x, y, "Komplek Ruko Puri Mestika, Blok I Nomor 6,  Batam Center, Batam.")
+    y -= 0.5 * cm
+
+    c.setFont("Times-Roman", 12)
+    c.drawString(x, y, "Telp. : 0778-4806-115 ; 0822-6848-0475")
+    y -= 0.5 * cm
+
+    c.setFont("Times-Roman", 12)
+    c.drawString(x, y, "Email : pt.alkatra@gmail.com; Website : https://pt-alkatra.business.site")
+    y -= 0.5 * cm
+
 
 def render_header(c, item, x, start_y):
+
     text_width = c.stringWidth("SLIP GAJI KARYAWAN", "Helvetica-Bold", 16)
     center = (width - text_width) / 2
     c.setFont("Helvetica-Bold", 16)
-    c.drawString(center, height - 3 * cm, "SLIP GAJI KARYAWAN")
+    c.drawString(center, start_y, "SLIP GAJI KARYAWAN")
+    y = start_y - 2 * cm
 
     c.setFont("Helvetica", 11)
-    c.drawString(x, start_y, "Nama")
-    c.drawString(x + 5 * cm, start_y, f": {item['nama']}")
-    y = start_y - 0.5 * cm
+    c.drawString(x, y, "Nama")
+    c.drawString(x + 5 * cm, y, f": {item['nama']}")
+    y -= 0.5 * cm
+    
+    c.drawString(x, y, "NIK")
+    c.drawString(x + 5 * cm, y, f": {item['nik']:.0f}")
+    y -= 0.5 * cm
 
-    c.setFont("Helvetica", 11)
     c.drawString(x, y, "Lokasi")
     c.drawString(x + 5 * cm, y, f": {item['lokasi']}")
     y -= 0.5 * cm
 
-    c.setFont("Helvetica", 11)
     c.drawString(x, y, "Status")
     c.drawString(x + 5 * cm, y, f": {item['status']}")
+    y -= 0.5 * cm
+    
+    c.drawString(x, y, "Periode Gaji")
+    c.drawString(x + 5 * cm, y, f": {bulan_indonesia[item['bulan'].month]} {item['bulan'].year}")
     y -= 0.5 * cm
 
     return y - 1 * cm
@@ -83,7 +122,7 @@ def render_penerimaan(c, item, x, start_y):
     c.drawString(x + 10 * cm, y, f": Rp. {item['cuti']:,.2f}")
     y -= 0.5 * cm
 
-    c.drawString(x, y, "Tanggal merah, Lembuh, Dll")
+    c.drawString(x, y, "Tanggal merah, Lembur, dll")
     c.drawString(x + 10 * cm, y, f": Rp. {(item['holiday'] + item['event']):,.2f}")
     y -= 0.5 * cm
 
@@ -104,9 +143,9 @@ def render_penerimaan(c, item, x, start_y):
     y -= 0.5 * cm
 
     c.setFont("Helvetica-Bold", 11)
-    total_penerimaan = item['basic'] + item['jabatan'] + item['cuti'] + item['holiday'] + item['event']+ item['lembur'] + item['revisi'] + item['pph_kantor']
+    item['total_penerimaan'] = item['basic'] + item['jabatan'] + item['cuti'] + item['holiday'] + item['event']+ item['lembur'] + item['revisi'] + item['pph_kantor']
     c.drawString(x, y, "TOTAL PENERIMAAN")
-    c.drawString(x + 10 * cm, y, f": Rp. {total_penerimaan:,.2f}")
+    c.drawString(x + 10 * cm, y, f": Rp. {item['total_penerimaan']:,.2f}")
 
     return y - 1 * cm
 
@@ -118,12 +157,12 @@ def render_potongan(c, item, x, start_y):
     c.setFont("Helvetica", 11)
 
     bpjs_kes = item['potongan_bpjskes'] - item['tunjangan_bpjskes']
-    c.drawString(x, y, "BPJS KESEHATAN")
+    c.drawString(x, y, "BPJS Kesehatan")
     c.drawString(x + 10 * cm, y, f": Rp. {bpjs_kes:,.2f}")
     y -= 0.5 * cm
 
     bpjs_tk = item['potongan_bpjstk'] - item['tunjangan_bpjstk']
-    c.drawString(x, y, "BPJS KESEHATAN")
+    c.drawString(x, y, "BPJS Ketenagakerjaan")
     c.drawString(x + 10 * cm, y, f": Rp. {bpjs_tk:,.2f}")
     y -= 0.5 * cm
     
@@ -141,25 +180,39 @@ def render_potongan(c, item, x, start_y):
     y -= 0.5 * cm
     
     c.setFont("Helvetica-Bold", 11)
-    total_potongan = bpjs_kes + bpjs_tk + item['kekurangan_jam'] + revisi + item['pph21']
+    item['total_potongan'] = bpjs_kes + bpjs_tk + item['kekurangan_jam'] + revisi + item['pph21']
     c.drawString(x, y, "TOTAL POTONGAN")
-    c.drawString(x + 10 * cm, y, f": Rp. {total_potongan:,.2f}")
+    c.drawString(x + 10 * cm, y, f": Rp. {item['total_potongan']:,.2f}")
 
     return y - 1* cm
 
-def render_footer(c, item, margin_x, start_y):
-    pass
+def render_footer(c, item, x, start_y):
+    c.setFont("Helvetica-Bold", 12)
+    item['total_diterima'] = item['total_penerimaan'] - item['total_potongan']
+    c.drawString(x, start_y, "TOTAL DITERIMA ")
+    angka = Decimal(str(item['total_diterima'])).quantize(Decimal("0.00"), rounding=ROUND_DOWN)
+    c.drawString(x + 10 * cm, start_y, f": Rp. {angka:,.2f}")
+    y = start_y - 2 * cm
+
+    c.setFont("Helvetica", 11)
+    c.drawString(x, y, "Mengetahui")
+    c.drawString(x, y - 3 * cm, "Joel Simatupang")
+    c.drawString(x, y - 3.5 * cm, "HRD")
+
 
 def multi_slip(data, filename="data/slip_gaji1.pdf"):
     c = canvas.Canvas(filename, pagesize=A4)
     width, height = A4
 
     for item in data:
+        render_kop(c)
+
         margin_x = 2 * cm
-        start_y = height - 5 * cm   # Inisialisasi bari bertama, kecuali judul
+        start_y = height - 4 * cm   # Inisialisasi bari bertama, kecuali judul
         start_y = render_header(c, item, margin_x, start_y)
         start_y = render_penerimaan(c, item, margin_x, start_y)
         start_y = render_potongan(c, item, margin_x, start_y)
+        start_y = render_footer(c, item, margin_x, start_y)
         c.showPage()
     c.save()
 
