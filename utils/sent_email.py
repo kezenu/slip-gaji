0542063f41404d2ea71375606aa743e0a2939c44
Wisnu Sanjaya <wsanjaya69@gmail.com>
2025-08-21 11:17:01 +0700

    Tyal Horse

--- utils/sent_email.py
+++ utils/sent_email.py
@@ -1,30 +1,55 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email import encoders
 from Include.passwordsmtp import pw_smtp
 
 
-# Data login Gmail
-email_pengirim = pw_smtp(2)
-app_password =  pw_smtp(1) # App password dari langkah 2
-email_penerima = "wsanjaya69@gmail.com"
+class PENGIRIM_EMAIL:
+    def __init__(self, penerima):
+        self.penerima = penerima
 
-# Buat pesan email
-msg = MIMEMultipart()
-msg["From"] = email_pengirim
-msg["To"] = email_penerima
-msg["Subject"] = "Test SMTP Gmail"
+    def kirim_email(self):
+        # Data login Gmail
+        email_pengirim = pw_smtp(2)
+        app_password =  pw_smtp(1) # App password dari langkah 2
+        email_penerima = self.penerima
 
-# Isi email
-body = "Halo, ini email percobaan via SMTP Gmail + Python!"
-msg.attach(MIMEText(body, "plain"))
+        # Buat pesan email
+        msg = MIMEMultipart()
+        msg["From"] = email_pengirim
+        msg["To"] = email_penerima
+        msg["Subject"] = f"slip gaji karyawan PT. Alkatra"
 
-try:
-    # Koneksi ke SMTP Gmail (pakai SSL port 465)
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login(email_pengirim, app_password)
-    server.sendmail(email_pengirim, email_penerima, msg.as_string())
-    server.quit()
-    print("✅ Email berhasil dikirim!")
-except Exception as e:
-    print("❌ Error:", e)
+        # Isi email
+        body = f"""
+    Kepada Yth. Bapak/Ibu Karyawan PT. ALKATRA
+
+    Berikut terlampir slip gaji bulan [Bulan] [Tahun].
+    Setiap file slip gaji di proteksi dengan NIK karyawan masing-masing.
+    Mohon diperiksa dan disimpan dengan baik.
+    Jika terdapat kesalahan atau keluhan, silakan hubungi bagian HRD.
+    Terima kasih.
+
+    Hormat saya,
+
+
+    [Nama Pengirim/HRD]
+    [Jabatan]
+    [Perusahaan]
+
+    """
+        msg.attach(MIMEText(body, "plain"))
+
+        try:
+            # Koneksi ke SMTP Gmail (pakai SSL port 465)
+            server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+            server.login(email_pengirim, app_password)
+            server.sendmail(email_pengirim, email_penerima, msg.as_string())
+            server.quit()
+            print("✅ Email berhasil dikirim!")
+        except Exception as e:
+            print("❌ Error:", e)
+
+email_kirim = PENGIRIM_EMAIL("wsanjaya69@gmail.com")
+email_kirim.kirim_email()
